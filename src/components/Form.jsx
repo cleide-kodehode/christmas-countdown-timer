7a61252957170c774c4e1151ebda522505c9d01a
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-01-13 08:55:40 +0100

    change nr 5

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,19 +6,25 @@ function Form() {
 const [comment, setComment] = useState("");
 const [comments, setComments] = useState([]);
 
-
 const onClickHandler = () => {
   setComments((comments) => [...comments, comment]);
+  setComment("");
 };
-const onChangeHandler = (e) => {
+
+const onChangeHandler = (e) =>  {
   setComment(e.target.value);
 };
+
 
   return (
     <div className="main-container">  
       <div className="coment-flexbox">
         <h3 className="comment-text"> 🎁🎅 Comment 💬 Christmas Messages & Wishes 🎅🎁 </h3>
-        <textarea value={comment} onChange={onChangeHandler} className="input-box"/>
+        <textarea value={comment} 
+                  onChange={onChangeHandler} 
+                  className="input-box" 
+                  placeholder="write your Christmas message here" 
+                  requirid />
         <button onClick={onClickHandler} className="comment-button"> Submit </button>
       </div>
       
